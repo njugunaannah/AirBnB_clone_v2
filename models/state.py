0385c5466e183fd4cd104eb1240e6d06562b8bc1
“Annah <njugunaannah@gmail.com>
2023-10-21 19:39:01 +0300

    edits

--- models/state.py
+++ models/state.py
@@ -3,10 +3,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
@@ -14,22 +13,18 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """
+            Get the list of City objects associated with this state
+            """
+            cities = models.storage.all(City)
+            return [city for city in cities.values()
+                    if city.state_id == self.id]
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
